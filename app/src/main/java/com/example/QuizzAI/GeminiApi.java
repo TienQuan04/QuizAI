83b5bf05a43a6813cb8356bfb8f7f0f8095c971a
Admin <tienquanpham999@gmail.com>
2026-04-18 09:50:23 +0700

    Fix Gemini API

--- app/src/main/java/com/example/QuizzAI/GeminiApi.java
+++ app/src/main/java/com/example/QuizzAI/GeminiApi.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 
 public class GeminiApi {
 
-    private static final String GEMINI_API_KEY = "keyyy";
+    private static final String GEMINI_API_KEY = "keyyyyyyyyy";
 
     private static final String GEMINI_API_URL =
             "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + GEMINI_API_KEY;
